6bfad0bb1c8be718a9b89761d6d5c11d4279edbe
Oliver Johnston <oliver@ojohnston.com>
2020-12-01 12:46:22 +0000

    tidy up

--- 1.py
+++ 1.py
@@ -1,17 +1,22 @@
-fp = open("1.txt")
+def part_1(data_set):
+    for i in data_set:
+        match = 2020 - i
+        if match in data_set:
+            return match * i
+
+
+def part_2(data_set):
+    for i in data_set:
+        for j in data_set:
+            match = 2020 - i - j
+            if match in data_set:
+                return i * j * match
+
+
+fp = open("input/1.txt")
 data = fp.read().splitlines()
 
 set = set([int(i) for i in data])
 
-# part 1
-for i in set:
-    match = 2020 - i;
-    if match in set:
-        print(i * match)
-
-# part 2
-for i in set:
-    for j in set:
-        match = 2020 - i - j;
-        if match in set:
-            print(i * j * match)
+print("Part 1: {}".format(part_1(set)))
+print("Part 2: {}".format(part_2(set)))
