a58ae4d4ea665a2a3ef9e93c8c8f500bbaa57c78
Oliver Johnston <oliver@ojohnston.com>
2020-12-01 12:34:45 +0000

    part 2

--- 1.py
+++ 1.py
@@ -3,8 +3,15 @@ data = fp.read().splitlines()
 
 set = set([int(i) for i in data])
 
+# part 1
 for i in set:
     match = 2020 - i;
     if match in set:
         print(i * match)
 
+# part 2
+for i in set:
+    for j in set:
+        match = 2020 - i - j;
+        if match in set:
+            print(i * j * match)
