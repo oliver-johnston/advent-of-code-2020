c7e87b58f4ee9588d3dec8a8ecbb83b9ebcf711b
Oliver Johnston <oliver@ojohnston.com>
2020-12-10 21:04:04 +0000

    comments

--- 10.py
+++ 10.py
@@ -1,34 +1,42 @@
 from collections import defaultdict
 
 fp = open("input/10.txt")
-data = [int(x) for x in fp.read().splitlines()]
+adapters = [int(x) for x in fp.read().splitlines()]
 
-data.append(max(data)+3)
-data.append(0)
+laptop = max(adapters)+3
+adapters.append(laptop)
+adapters.append(0)
 
-data.sort()
-
+adapters.sort()
 
 diffs = defaultdict(lambda: 0)
-for i in range(0, len(data)-1):
-    diff = data[i+1] - data[i]
+for i in range(0, len(adapters)-1):
+    diff = adapters[i+1] - adapters[i]
     diffs[diff] += 1
 
 print("Part 1: {}".format(diffs[1] * diffs[3]))
 
-seqs = defaultdict(lambda: 0)
+# contains the number of ways to get to each adapter
+combinations = defaultdict(lambda: 0)
+# there is only one way to start with the socket
+combinations[0] = 1
 
-for x in data:
+for current_adapter in adapters[1:]:
+    for previous_adapter in list(combinations.keys()):
 
-    if len(seqs) == 0:
-        seqs[x] = 1
-    else:
-        for y in list(seqs.keys()):
-            diff = x - y
-            if diff <= 3:
-                seqs[x] += seqs[y]
-            if diff >= 3:
-                seqs.pop(y)
+        diff = current_adapter - previous_adapter
 
-print("Part 2: {}".format(sum(seqs.values())))
+        # If the difference between adapters is less than or equal to 3 then it is possible to plug the
+        # previous adapter into the current adapter. This means that every combination that allowed us
+        # to get us to the previous adapter will also allow us to get us to the current adapter.
+        if diff <= 3:
+            combinations[current_adapter] += combinations[previous_adapter]
+
+        # If the difference between the adapters is more than 3 then it is not possible to plug the
+        # previous adapter into the current adapter. This means it is not possible to use that path
+        # to get to the end so we discard it.
+        if diff > 3:
+            combinations.pop(previous_adapter)
+
+print("Part 2: {}".format(combinations[laptop]))
 
